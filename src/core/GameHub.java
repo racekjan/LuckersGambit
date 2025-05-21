371fe1285efacb4c66696afd2165b235e7692f1e
Tomas Vovesny <114948131+TomasVOV@users.noreply.github.com>
2025-05-21 09:31:02 +0200

    změna bet

--- src/core/GameHub.java
+++ src/core/GameHub.java
@@ -6,7 +6,7 @@ import ui.InputHandler;
 
 public class GameHub {
     Player player;
-    static Game[] games = {new Roulette("Roulette", 1500), new DiceRoll("Dice Roll", 1000)};
+    static Game[] games = {new Roulette("Roulette", 1500), new DiceRoll("Dice Roll", 2000)};
 
 
     public Player getPlayer() {
